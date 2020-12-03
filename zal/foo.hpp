43df012718a64f7bc167dd624f254eec9879b8b6
KontradZebrowsk <konzebr@gmail.com>
2020-12-03 21:35:11 +0000

    const_iterator

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -8,22 +8,22 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    std::vector<char> kotofil;
-for(Human e : people)
-{
-  e.birthday();
-}
-for(std::list< Human >::reverse_iterator it = people.rbegin(); it!= people.rend(); it++)
-{
-if(it->isMonster() == true)
+  std::vector<char> kotofil;
+  for(Human e : people)
   {
-  kotofil.emplace_back("n");
+    e.birthday();
   }
-else
+  for(std::list< Human >::const_reverse_iterator it = people.crbegin(); it!= people.crend(); it++)
   {
-  kotofil.emplace_back("y");
-  }
+    if(it->isMonster() == true)
+    {
+    kotofil.emplace_back("n");
+    }
+    else
+    {
+    kotofil.emplace_back("y");
+    }
 
-}
+  }
     return kotofil;
 }
